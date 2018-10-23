d43e52bd1735ad95d76092aedb008f3900d333f6
mergwyn <gary@theclarkhome.com>
2018-10-23 15:28:33 +0100

    Refactor to use a link

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,25 +1,35 @@
 #
 class radarr::install {
+  #TODO tidy up old versions
 
-  $install_path   = $::radarr::install_path
-  $package_name   = 'Radarr.develop'
+  $package_name    = 'Radarr.develop'
   $package_version = $::radarr_version
-  $repository_url = 'https://github.com/Radarr/Radarr/releases/download/'
-  $package_source = "${repository_url}/${package_version}/${package_name}.tar.gz"
-  $archive_name   = "${install_path}/${package_name}-${package_version}.tar.gz"
+  $install_path    = $::radarr::install_path
+  $extract_dir     = "${install_path}/Radarr-${package_version}"
+  $creates         = "${extract_dir}/Radarr"
+  $link            = "${install_path}/Radarr"
+  $repository_url  = 'https://github.com/Radarr/Radarr/releases/download/'
+  $package_source  = "${repository_url}/${package_version}/${package_name}.tar.gz"
+  $archive_name    = "${package_name}-${package_version}.tar.gz"
+  $archive_path    = "${install_path}/${archive_name}"
 
   if $radarr::package_manage {
-    archive { 'radarr_package_tar':
-      path         => $archive_name,
+    file { $extract_dir: ensure => directory, }
+
+    archive { $archive_name:
+      path         => $archive_path,
       source       => $package_source,
-      user         => $::radarr::user,
-      group        => $::radarr::group,
       extract      => true,
-      extract_path => $install_path,
-      #creates      => $archive_name,
+      extract_path => $extract_dir,
+      creates      => $creates,
       cleanup      => false,
       #TODO reference to mono
       #require      => Class['mono'],
+    }
+    file { $link:
+      ensure    => 'link',
+      target    => $creates,
+      subscribe => Archive[$archive_name],
     }
   }
 }
