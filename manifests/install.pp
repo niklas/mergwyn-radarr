2266d0b5d7b553c9f4dfde84b7872cf05eb7900e
mergwyn <gary@theclarkhome.com>
2018-11-12 15:49:11 +0000

    Add debugging and align with jackett

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,7 @@
 #
 class radarr::install {
   #TODO tidy up old versions
-
+  notice("radarr_version is '${::radarr_version}'")
   $package_name    = 'Radarr.develop'
   $package_version = $::radarr_version
   $install_path    = $::radarr::install_path
@@ -26,17 +26,25 @@ class radarr::install {
       extract      => true,
       extract_path => $extract_dir,
       creates      => $creates,
-      cleanup      => false,
+      cleanup      => true,
       user         => $::radarr::user,
       group        => $::radarr::group,
       #TODO reference to mono
       #require      => Class['mono'],
+      notify       => Service['radarr.service']
     }
     file { $link:
       ensure    => 'link',
       target    => $creates,
       subscribe => Archive[$archive_name],
     }
+    exec {'radarr_tidy':
+      cwd         => $install_path,
+      path        => '/usr/sbin:/usr/bin:/sbin:/bin:',
+      command     => "ls -d ${link}-* | head -n +${keep} | xargs rm -rf",
+      #onlyif      => "test $(ls -d ${link}-* | wc -l) -gt ${keep}",
+      refreshonly => true,
+      subscribe   => Archive[$archive_name],
   }
 }
 # vim: number tabstop=8 expandtab shiftwidth=2 softtabstop=2
