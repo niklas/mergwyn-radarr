b78c4c077818265081fb29feeec40409482d1d71
mergwyn <gary@theclarkhome.com>
2018-11-12 18:14:16 +0000

    change to tags rather than release

--- lib/facter/radarr_version.rb
+++ lib/facter/radarr_version.rb
@@ -14,7 +14,7 @@ Facter.add(:radarr_version) do
     confine :kernel  => :linux
     setcode do
         #radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/releases/latest -O - | grep -E \/tag\/ | head -1 | sed 's:^.*/v\([0-9\.]*\)\".*:\1:'")
-        radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/releases/latest -O - | grep -E \/tag\/ | head -1 |  awk -F '[/<>vi\"]' '{print $9 }'")
+        radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/tags -O - | grep -E \/tag\/ | head -1 |  awk -F '[/<>vi\"]' '{print $9 }'")
 	if radarr_version == ""
 	    nil
 	else
