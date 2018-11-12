3ee9a141d149b01965ed46c133f9279dda02b187
mergwyn <gary@theclarkhome.com>
2018-11-12 17:19:20 +0000

    use awk not sed

--- lib/facter/radarr_version.rb
+++ lib/facter/radarr_version.rb
@@ -13,7 +13,8 @@ end
 Facter.add(:radarr_version) do
     confine :kernel  => :linux
     setcode do
-        radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/releases/latest -O - | grep -E \/tag\/ | head -1 | sed 's:^.*/v\([0-9\.]*\)\".*:\1:'")
+        #radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/releases/latest -O - | grep -E \/tag\/ | head -1 | sed 's:^.*/v\([0-9\.]*\)\".*:\1:'")
+        radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/releases/latest -O - | grep -E \/tag\/ | head -1 |  awk -F '[/<>vi\"]' '{print $9 }'")
 	if radarr_version == ""
 	    nil
 	else
