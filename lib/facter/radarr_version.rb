a8aa870644734205e1e3d0c6c9549e883ec36542
mergwyn <gary@theclarkhome.com>
2018-11-12 16:47:28 +0000

    correct conditional to ==

--- lib/facter/radarr_version.rb
+++ lib/facter/radarr_version.rb
@@ -14,7 +14,7 @@ Facter.add(:radarr_version) do
     confine :kernel  => :linux
     setcode do
         radarr_version = Facter::Util::Resolution.exec("wget -q https://github.com/Radarr/Radarr/releases/latest -O - | grep -E \/tag\/ | head -1 | awk -F '[><]' '{print $3}' | sed 's:^.*/v\([0-9\.]*\)\".*:\1:'")
-	if radarr_version = ""
+	if radarr_version == ""
 	    nil
 	else
 	    radarr_version
