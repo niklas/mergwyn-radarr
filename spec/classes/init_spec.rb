186c2f2e7455a97801c2bafb1ca7579dbd717396
mergwyn <gary@theclarkhome.com>
2018-12-17 19:02:18 +0000

    Loop test for each supported os

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 describe 'radarr' do
-  context 'with default values for all parameters' do
-    it { is_expected.to contain_class('radarr') }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      let(:pre_condition) { 'include archive' }
+
+      context 'with default values for all parameters' do
+	it { is_expected.to contain_class('radarr') }
+      end
+    end
   end
 end
